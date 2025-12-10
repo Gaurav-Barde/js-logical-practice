0ad48e49d76803471c0b9dcd9d115c41df414aa6
Gaurav-Barde <gabarde25@gmail.com>
2025-12-10 23:11:31 +0530

    💡 Solve JS logical problem(2025-12-10): Print Butterfly Pattern(Improved)

--- problems/2025-12-10/Problem1/brute-force.js
+++ problems/2025-12-10/Problem1/brute-force.js
@@ -10,21 +10,19 @@
 // **      **
 // *        *
 
-const rows = 8;
+const rows = 3;
 
 const printButterflyPattern = (rows) => {
   const width = rows * 2 - 1;
   const arr = Array(width).fill(" ");
-
   for (let i = 0; i < width; i++) {
     if (i >= rows) {
       arr[width - i] = " ";
       arr[i] = " ";
-      console.log(arr.join(""));
-      continue;
+    } else {
+      arr[i] = "*";
+      arr[width - i] = "*";
     }
-    arr[i] = "*";
-    arr[width - i] = "*";
     console.log(arr.join(""));
   }
 };
