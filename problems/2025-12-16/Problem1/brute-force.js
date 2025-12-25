278269b4d608d12fb8d3b0dcc6e5c5ee8151be2e
Gaurav-Barde <gabarde25@gmail.com>
2025-12-25 19:57:07 +0530

    💡 Solve JS logical problem(2025-12-16): Generate a Decimal Number from Whole and Fractional Digits

--- problems/2025-12-16/Problem1/brute-force.js
+++ problems/2025-12-16/Problem1/brute-force.js
@@ -2,31 +2,38 @@
 // eg. input: Whole = [1,2], Fraction=[3,4]
 // Output: 12.34
 
-const whole = [11, 22, 33, 88];
-const fraction = [3, 4, 8, 9, 8, 9];
+const whole = [1, 2];
+const fraction = [3, 4];
 
 const generateDecimalFromWholeAndFraction = (whole, fraction) => {
-  let pow = 0;
+  let powWhole = 0;
   let wholePart = 0;
   for (let i = whole.length - 1; i > -1; i--) {
-    if (whole[i] > 10) {
-      wholePart += (whole[i] % 10) * 10 ** pow;
-      pow++;
-      wholePart += Math.floor(whole[i] / 10) * 10 ** pow;
-      pow++;
-    } else {
-      wholePart += whole[i] * 10 ** pow;
-      pow++;
+    let current = whole[i];
+    while (current > 9) {
+      let reminder = current % 10;
+      wholePart += reminder * 10 ** powWhole;
+      powWhole++;
+      current = (current - reminder) / 10;
     }
+    wholePart += current * 10 ** powWhole;
+    powWhole++;
   }
 
-  let powFr = -1;
-  let fractionPart = 0.0;
+  let powFra = -1;
+  let fractionPart = 0;
   for (let i = 0; i < fraction.length; i++) {
-    fractionPart += fraction[i] * 10 ** powFr;
-    powFr--;
+    let current = fraction[i];
+    while (current > 9) {
+      let reminder = Math.floor(current / 10);
+      fractionPart += reminder * 10 ** powFra;
+      powFra--;
+      current = current % 10;
+    }
+    fractionPart += current * 10 ** powFra;
+    powFra--;
   }
-  fractionPart = +fractionPart.toFixed(fraction.length);
+
   return wholePart + fractionPart;
 };
 
