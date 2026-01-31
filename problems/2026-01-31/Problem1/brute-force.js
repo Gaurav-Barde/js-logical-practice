46ace2e755536936859a3763dc0e2048df23e20e
Gaurav-Barde <gabarde25@gmail.com>
2026-01-31 23:12:57 +0530

    💡 Solve JS logical problem(2025-01-31): Print All Factors of a Number

---
+++ problems/2026-01-31/Problem1/brute-force.js
@@ -0,0 +1,19 @@
+// 🧩 Problem: Print All Factors of a Number
+// Input: N = 24
+// output: [1,2,3,4,6,8,12,24]
+
+const num = 24;
+
+const printAllFactorsOfANumber = (num) => {
+  const factors = [];
+
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+  return factors;
+};
+
+const result = printAllFactorsOfANumber(num);
+console.log(result);
