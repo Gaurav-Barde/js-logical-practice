603d8becc84a0facb49e78fa7fec9dbbe6a0f0a0
Gaurav-Barde <gabarde25@gmail.com>
2026-02-16 00:43:31 +0530

    💡 Solve JS logical problem(2025-02-15): Find the LCM (Least Common Multiple) of Two Numbers

--- problems/2026-02-15/Problem1/brute-force.js
+++ problems/2026-02-15/Problem1/brute-force.js
@@ -1,20 +1,27 @@
 // 🧩 Problem: Find the LCM (Least Common Multiple) of Two Numbers
 // Input: 12, 18
 // output: LCM = 36
+// Reason: 36 is the least number which is divisible by 12 & 18
 
 const num1 = 12,
   num2 = 18;
 
 const findLCMOfTwoNumbers = (num1, num2) => {
-  let temp = num;
-  while (temp < num2) {
-    if (num1 % temp === 0 && num2 % temp === 0) {
-      commonFactors.push(temp);
-    }
-    temp++;
+  let highest = num1 > num2 ? num1 : num2;
+  let lowest = num1 === highest ? num2 : num1;
+
+  if (highest % lowest === 0) {
+    return highest;
   }
-  return commonFactors[commonFactors.length - 1];
+
+  let multiplier = 2;
+
+  while ((highest * multiplier) % lowest !== 0) {
+    multiplier++;
+  }
+
+  return highest * multiplier;
 };
 
-const result = findHCFOfTwoNumbers(num1, num2);
+const result = findLCMOfTwoNumbers(num1, num2);
 console.log(result);
