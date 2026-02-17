275e297f9901fed67070d40f3cb837b3f59c938b
Gaurav-Barde <gabarde25@gmail.com>
2026-02-17 23:59:25 +0530

    💡 Solve JS logical problem(2025-02-17): started on a problem

---
+++ problems/2026-02-17/Problem1/brute-force.js
@@ -0,0 +1,15 @@
+// 🧩 Problem: Calculate the Sum of All Factors of a Number
+// Input: N = 12
+// output: sum = 28
+// Reason: 1 + 2 + 3 + 4 + 6 + 12 = 28
+
+const num = 12;
+
+const sumOfAllFactors = (num) => {
+  let sum = num;
+
+  let temp = num % 2 === 0 ? num % 2 : (num % 2) - 1;
+};
+
+const result = findLCMOfTwoNumbers(num1, num2);
+console.log(result);
